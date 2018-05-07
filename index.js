9615519264ffdc050c110629877742679ba89b42
Alexander Nilsson <alexander@alxoptr.se>
2018-05-07 18:14:27 +0200

    Try to detect package.json for project and determine defaults for TS and Storybook

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const path = require('path')
 const inquirer = require('inquirer')
+const fs = require('fs')
 
 const dir = process.argv[2]
 
@@ -8,38 +9,65 @@ if (!dir) {
     return console.error('No directory defined')
 }
 
+const packageDir = path.resolve(process.env.PWD, 'package.json')
+
+let hasStorybook = false
+let hasTypeScript = false
+
+if (fs.existsSync(packageDir)) {
+    const pkg = require(packageDir)
+
+    if (pkg.dependencies) {
+        hasStorybook = pkg.dependencies['@storybook/react']
+            ? true
+            : hasStorybook
+        hasTypeScript = pkg.dependencies['typescript'] ? true : hasTypeScript
+    }
+
+    if (pkg.devDependencies) {
+        hasStorybook = pkg.devDependencies['@storybook/react']
+            ? true
+            : hasStorybook
+        hasTypeScript = pkg.devDependencies['typescript'] ? true : hasTypeScript
+    }
+}
+
 const createComponent = require('./createComponent')
 
+const prompt = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Component name'
+    },
+    {
+        type: 'list',
+        name: 'ts',
+        message: 'TypeScript?',
+        choices: ['No', 'Yes'],
+        default: hasTypeScript ? 'Yes' : 'No'
+    },
+    {
+        type: 'list',
+        name: 'type',
+        message: 'Type of component?',
+        choices: ['React.Component', 'React.PureComponent', 'Function']
+    },
+    {
+        type: 'list',
+        name: 'stylesheet',
+        message: 'Stylesheet?',
+        choices: ['No', 'SCSS', 'SCSS Module', 'CSS', 'CSS Module']
+    },
+    {
+        type: 'list',
+        name: 'story',
+        message: 'Storybooks story?',
+        choices: ['No', 'Yes'],
+        default: hasStorybook ? 'Yes' : 'No'
+    }
+]
+
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Component name'
-        },
-        {
-            type: 'list',
-            name: 'ts',
-            message: 'TypeScript?',
-            choices: ['No', 'Yes']
-        },
-        {
-            type: 'list',
-            name: 'type',
-            message: 'Type of component?',
-            choices: ['React.Component', 'React.PureComponent', 'Function']
-        },
-        {
-            type: 'list',
-            name: 'stylesheet',
-            message: 'Stylesheet?',
-            choices: ['No', 'SCSS', 'SCSS Module', 'CSS', 'CSS Module']
-        },
-        {
-            type: 'list',
-            name: 'story',
-            message: 'Storybooks story?',
-            choices: ['No', 'Yes']
-        }
-    ])
+    .prompt(prompt)
     .then(input => createComponent(path.join(process.cwd(), dir), input))
